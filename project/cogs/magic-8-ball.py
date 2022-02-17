6335204e444cbf034bcbab8c0673f703e9724b06
Corban-Lee <843224@student.derby-college.ac.uk>
2022-02-17 13:59:20 +0000

    Various changes (see description)
    Added database integrity check that checks the existence of the needed table and rows.
    
    Made command usable outside of test server.
    
    Changed ask_question command to work with new database utility.

--- project/cogs/magic-8-ball.py
+++ project/cogs/magic-8-ball.py
@@ -8,6 +8,51 @@ class Magic8Ball(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        # check integrity of database for magic 8 ball
+        self.database_integrity_check()
+
+
+
+    def database_integrity_check(self):
+        self.bot.db_utils.create_table(
+            table_name='Magic8BallResponses',
+            columns=(
+                'id integer PRIMARY KEY',
+                'content VARCHAR(80) NOT NULL',
+                'isPositive BOOL NOT NULL',
+            )
+        )
+        if len(self.bot.db_utils.get_row(
+            table_name='Magic8BallResponses',
+            values='*'
+        )) == 0:
+            for id, default in enumerate(
+                (
+                    ('It is certain', True),
+                    ('Without a doubt', True),
+                    ('Yes definitely', True),
+                    ('You may rely on it', True),
+                    ('As I see it, yes', True),
+                    ('Most likely', True),
+                    ('Outlook good', True),
+                    ('Yes', True),
+                    ('Reply hazy, try again', False),
+                    ('Ask again later', False),
+                    ('Better not tell you now', False),
+                    ('Cannot predict now', False),
+                    ('Concentrate and try again', False),
+                    ("Don't count on it", False),
+                    ('My reply is no', False),
+                    ('My sources say no', False),
+                    ('Outlook not so good', False),
+                    ('Very doubtful', False),
+                )
+            ):
+                self.bot.db_utils.insert_row(
+                    table_name='Magic8BallResponses',
+                    values=(id, default[0], default[1])
+                )
+
 
     @staticmethod
     @commands.Cog.listener()
@@ -18,7 +63,7 @@ class Magic8Ball(commands.Cog):
     @cog_ext.cog_slash(
         name='Magic-8-Ball',
         description='Have your questions answered',
-        guild_ids=[819325370087112744],
+        guild_ids=[],
         options = [
             create_option(
                 name='question',
@@ -29,8 +74,8 @@ class Magic8Ball(commands.Cog):
         ]
     )
     async def ask_question(self, context:SlashContext, question):
-        answer = tuple(self.bot.db_utils.get('Magic8BallResponses', '*', 'RANDOM() LIMIT 18'))[0][1]
-        reply = f'You asked: {question}\nMy response is: {answer}'
+        answer = tuple(self.bot.db_utils.get_row('Magic8BallResponses', '*', additional_arg='ORDER BY RANDOM() LIMIT 18'))[0][1]
+        reply = f'*You asked:*   {question}\n*My response is:*   {answer}'
         await context.reply(reply)
 
 
